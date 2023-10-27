b929d9c8998632a3c18e3d17ad823fe198b321a6
Shlok Phulkar <shlok33phulkar@gmail.com>
2023-10-27 14:18:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,1 @@
 import streamlit
-streamlit.title('My parents new health dinner')
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
